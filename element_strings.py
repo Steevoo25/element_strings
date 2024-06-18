4584e318e84cb1334bb41438af69c9d0fc6b1194
hjs115 <hjs115@student.bham.ac.uk>
2024-06-18 21:49:36 +0100

    refactor for clarity

--- element_strings.py
+++ element_strings.py
@@ -1,8 +1,11 @@
 # open file
-symbols_file = open("symbols.txt", "r")
-data = symbols_file.read()
+elements_file = open("elements.txt", "r")
+elements = elements_file.read()
 # extract symbols
-elements = data.split("\n")
+elements = elements.split("\n")
 symbols = []
 for element in elements:
     symbols.append(element.split(",")[1])
+symbols_file = open("symbols.txt", "w")
+symbols_file.write(symbols.__str__)
+print(symbols)
