660a924d115052bcb40b3fe437ef3fbe0d7350f1
hjs115 <hjs115@student.bham.ac.uk>
2024-06-18 21:54:18 +0100

    Changed to use dict for storing symbols

--- element_strings.py
+++ element_strings.py
@@ -3,6 +3,16 @@ elements_file = open("elements.txt", "r")
 elements = elements_file.read()
 # extract symbols
 elements = elements.split("\n")
-symbols = []
+
+symbols = {}
 for element in elements:
-    symbols.append(element.split(",")[1])
+    element = element.split(",")
+    symbols[element[1]] = element [0]
+
+print(symbols)
+
+def is_string_possible(word: str) -> bool:
+    return False
+
+def create_string_from_element_symbols(word: str) -> list:
+    return 
