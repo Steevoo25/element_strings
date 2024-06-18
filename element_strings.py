b161d696a025a1ccd6add16f37df6387b24f3634
hjs115 <hjs115@student.bham.ac.uk>
2024-06-18 21:50:18 +0100

    remove writing to symbols file

--- element_strings.py
+++ element_strings.py
@@ -6,6 +6,3 @@ elements = elements.split("\n")
 symbols = []
 for element in elements:
     symbols.append(element.split(",")[1])
-symbols_file = open("symbols.txt", "w")
-symbols_file.write(symbols.__str__)
-print(symbols)
