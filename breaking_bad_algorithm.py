1d003fc9ba1ffde309b2664ac9a9bb270902ec7c
hjs115 <hjs115@student.bham.ac.uk>
2024-06-18 22:40:40 +0100

    Added edge case

--- breaking_bad_algorithm.py
+++ breaking_bad_algorithm.py
@@ -6,14 +6,13 @@ elements = elements.split("\n")
 
 single_symbols = {}
 double_symbols = {}
-
+# 
 for element in elements:
     element = element.split(",")
     if len(element[1]) == 1:
         single_symbols[element[1]] = element[0]
     else:
         double_symbols[element[1]] = element[0]
-
 
 def check_single_symbol(word: str):  
     return word in single_symbols.keys()
@@ -24,16 +23,25 @@ def check_double_symbol(word: str):
 def create_string_from_element_symbols(word: str):
     pos = 0
     elements_to_create = []
+    previous_was_single = False
     while pos < len(word):
-        if check_double_symbol(word[pos:pos+2].capitalize()):
-            elements_to_create.append(double_symbols[word[pos:pos+2].capitalize()])
-            pos +=2
-        elif check_single_symbol(word[pos]):
+        if check_single_symbol(word[pos]):
             elements_to_create.append(single_symbols[word[pos]])
             pos +=1
+            previous_was_single = True
+        elif check_double_symbol(word[pos:pos+2].capitalize()):
+            elements_to_create.append(double_symbols[word[pos:pos+2].capitalize()])
+            pos +=2
+            previous_was_single = False
         else:
-            print("Word not possible:", word)
-            return None
+            # Case where a single symbol has been selected but cannot go any further, so check double symbols
+            if previous_was_single:
+                if check_double_symbol(word[pos-1:pos+1].capitalize()):
+                    elements_to_create.pop(len(elements_to_create)-1)
+                    elements_to_create.append(double_symbols[word[pos-1:pos+1].capitalize()])
+                    pos +=1
+                    previous_was_single = False
+            else: return None
     return elements_to_create
 
 if __name__ == "__main__":
@@ -41,4 +49,8 @@ if __name__ == "__main__":
     while not valid_word:
         word = input("Please enter a word\n").upper().strip()
         valid_word = word.isalpha()
-    create_string_from_element_symbols(word)
+    result = create_string_from_element_symbols(word)
+    if result == None:
+        print("Word not possible:", word)
+    else:
+        print(result, "can be used to create the word", word)
