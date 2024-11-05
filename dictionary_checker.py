150f78ebb04fd2ce8ae06550f7fbb3a7285e77c5
hjs115 <hjs115@student.bham.ac.uk>
2024-11-05 21:13:35 +0000

    Initiated structure for dictionary data analysis

---
+++ dictionary_checker.py
@@ -0,0 +1,20 @@
+# This script runs the breaking bad algorithm on a csv containing words, and performs some analysis on the results
+
+from breaking_bad_algorithm import create_string_from_element_symbols
+from matplotlib import pyplot
+import pandas as pd
+
+# -- STRUCTURE--
+
+#data cleaning
+#1 remvoe duplicates
+#2 remove spaces
+#3 remove special characters "-, ', "
+
+# import csv into df
+# df structure:
+# word | languague | length | BrBa Result
+
+# run words through BrBa and save results into df
+
+# plot results
