bf64acc72c8ad38bb1ffb0d685981c379254ce71
hjs115 <hjs115@student.bham.ac.uk>
2024-11-05 21:49:25 +0000

    open dictionary file

--- dictionary_checker.py
+++ dictionary_checker.py
@@ -1,11 +1,19 @@
 # This script runs the breaking bad algorithm on a csv containing words, and performs some analysis on the results
 
 from breaking_bad_algorithm import create_string_from_element_symbols
-from matplotlib import pyplot
+from matplotlib import pyplot as plt
 import pandas as pd
+
+import csv
 
 # -- STRUCTURE--
 
+path : str = './Dictionaries/English/OPTED-Dictionary.csv'
+
+with open(path, 'r', newline='') as csv_file:
+    csv_data = csv.reader(csv_file, delimiter=',')
+    for row in csv_data:
+        print(row)
 #data cleaning
 #1 remvoe duplicates
 #2 remove spaces
